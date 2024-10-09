01d104428f926fef85e29b73be45fd3f60690ebf
szymptom <bpodkolzin@kse.org.ua>
2024-10-09 21:09:54 +0300

    user secerts & provide basic code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace yt_downloader;
+
+class Program
+{
+    private static ITelegramBotClient bot;
+    public static async Task Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddUserSecrets<Program>()
+            .Build();
+
+        bot = new TelegramBotClient(config["BotToken"]);
+        
+        using var cts = new CancellationTokenSource();
+
+        var receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = []
+        };
+
+        bot.StartReceiving(
+            HandleUpdateAsync,
+            HandleErrorAsync,
+            receiverOptions,
+            cancellationToken: cts.Token
+        );
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await cts.CancelAsync();
+    }
+
+    private static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        var errorMessage = exception switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        Console.WriteLine(errorMessage, Console.BackgroundColor == ConsoleColor.DarkRed);
+        return Task.CompletedTask;
+    }
+
+    private static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        if (update is { Type: UpdateType.Message, Message.Text: not null })
+        {
+            var message = update.Message;
+            Console.WriteLine($"Received a message from {message.Chat.Id}: {message.Text}", Console.BackgroundColor == ConsoleColor.DarkYellow);
+
+            await botClient.SendTextMessageAsync(
+                chatId: message.Chat.Id,
+                text: $"I am parrot: {message.Text}",
+                cancellationToken: cancellationToken
+            );
+        }
+    }
+}
